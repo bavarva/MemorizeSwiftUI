6b11e200aa9cd40f187315188a9f122baa7ce497
ARNAV ASHOK BAVARVA <arnavbavarva@icloud.com>
2025-07-14 17:33:14 +0530

    cards made with array

--- CS193p_01/ContentView.swift
+++ CS193p_01/ContentView.swift
@@ -8,12 +8,13 @@
 import SwiftUI
 
 struct ContentView: View {
+    let emojis: [String] = ["😎","😀","😄","😁"]
     var body: some View {
         HStack {
-            CardView(isFaceUp: true)
-            CardView()
-            CardView(isFaceUp: true)
-            CardView()
+            ForEach(emojis.indices, id:\.self){ index in
+                CardView(content:emojis[index],isFaceUp: true)
+            }
+            
         }
         .foregroundColor(.cyan)
         .padding()
@@ -22,6 +23,7 @@ struct ContentView: View {
 
 
 struct CardView: View{
+    let content: String
     @State var isFaceUp = false
     
     var body: some View{
@@ -30,7 +32,7 @@ struct CardView: View{
             if isFaceUp{
                 base.fill(.white)
                 base.strokeBorder(lineWidth: 2)
-                Text("😎").font(.largeTitle)
+                Text(content).font(.largeTitle)
             }
             else{
                 base.fill()
